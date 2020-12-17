ef2567fa3d96478ca69a60ae5e8fc29a030a5139
Arnaud GAY <arnaud.gay@ens.uvsq.fr>
2020-12-17 10:19:42 +0100

    matteo

--- calculatrice.py
+++ calculatrice.py
@@ -6,30 +6,33 @@ def addition():
     return
 
 def multiplication():
+    pass
 
 
 def division():
+    pass
     
 
 def soustraction():
+    pass
 
 
 # interface:
 racine =  tk.Tk()
 canvas = tk.Canvas(racine, bg="black", height=HEIGHT, width=WIDTH)
 canvas.grid(column=0, row=0)
-number1 = tk.Button(racine, text="1",font=("30"), command=demCouleur)
-number2 = tk.Button(racine, text="2",font=("30"), command=demCouleur)
-number3 = tk.Button(racine, text="3",font=("30"), command=demCouleur)
-number4 = tk.Button(racine, text="4",font=("30"), command=demCouleur)
-number5 = tk.Button(racine, text="5",font=("30"), command=demCouleur)
-number6 = tk.Button(racine, text="6",font=("30"), command=demCouleur)
-number7 = tk.Button(racine, text="7",font=("30"), command=demCouleur)
-number8 = tk.Button(racine, text="8",font=("30"), command=demCouleur)
-number9 = tk.Button(racine, text="9",font=("30"), command=demCouleur)
-plus = tk.Button(racine, text="+",font=("30"), command=demCouleur)
-moins = tk.Button(racine, text="-",font=("30"), command=demCouleur)
-diviser = tk.Button(racine, text="/",font=("30"), command=demCouleur)
-multiplication = tk.Button(racine, text="*",font=("30"), command=demCouleur)
+number1 = tk.Button(racine, text="1",font=("30"), command=add1)
+number2 = tk.Button(racine, text="2",font=("30"), command=add2)
+number3 = tk.Button(racine, text="3",font=("30"), command=add3)
+number4 = tk.Button(racine, text="4",font=("30"), command=add4)
+number5 = tk.Button(racine, text="5",font=("30"), command=add5)
+number6 = tk.Button(racine, text="6",font=("30"), command=add6)
+number7 = tk.Button(racine, text="7",font=("30"), command=add7)
+number8 = tk.Button(racine, text="8",font=("30"), command=add8)
+number9 = tk.Button(racine, text="9",font=("30"), command=add9)
+plus = tk.Button(racine, text="+",font=("30"), command=addition)
+moins = tk.Button(racine, text="-",font=("30"), command=multiplication)
+diviser = tk.Button(racine, text="/",font=("30"), command=division)
+multiplication = tk.Button(racine, text="*",font=("30"), command=soustraction)
 # mainloop.
 racine.mainloop()
