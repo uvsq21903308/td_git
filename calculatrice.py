2a9270a885de339ef2430ce21704d4c4609e3023
Arnaud GAY <arnaud.gay@ens.uvsq.fr>
2020-12-17 14:51:03 +0100

    add pavé numérique et base fct

--- calculatrice.py
+++ calculatrice.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+# Varioables:
+HEIGHT, WIDTH = 100, 300
 # fonction:
 def addition():
 
@@ -17,19 +19,68 @@ def soustraction():
     pass
 
 
+def add1():
+    pass
+
+
+def add2():
+    pass
+
+
+def add3():
+    pass
+
+
+def add4():
+    pass
+
+
+def add5():
+    pass
+
+
+def add6():
+    pass
+
+
+def add7():
+    pass
+
+
+def add8():
+    pass
+
+
+def add9():
+    pass
+
+
 # interface:
 racine =  tk.Tk()
 canvas = tk.Canvas(racine, bg="black", height=HEIGHT, width=WIDTH)
-canvas.grid(column=0, row=0)
+canvas.grid(column=0, row=0, columnspan=4)
+
+#Pavé numérique:
 number1 = tk.Button(racine, text="1",font=("30"), command=add1)
+number1.grid(column=0, row=1)
 number2 = tk.Button(racine, text="2",font=("30"), command=add2)
+number2.grid(column=1, row=1)
 number3 = tk.Button(racine, text="3",font=("30"), command=add3)
+number3.grid(column=2, row=1)
 number4 = tk.Button(racine, text="4",font=("30"), command=add4)
+number4.grid(column=0, row=2)
 number5 = tk.Button(racine, text="5",font=("30"), command=add5)
+number5.grid(column=1, row=2)
 number6 = tk.Button(racine, text="6",font=("30"), command=add6)
+number6.grid(column=2, row=2)
 number7 = tk.Button(racine, text="7",font=("30"), command=add7)
+number7.grid(column=0, row=3)
 number8 = tk.Button(racine, text="8",font=("30"), command=add8)
+number8.grid(column=1, row=3)
 number9 = tk.Button(racine, text="9",font=("30"), command=add9)
+number9.grid(column=2, row=3)
+
+# Opérations:
 plus = tk.Button(racine, text="+",font=("30"), command=addition)
 moins = tk.Button(racine, text="-",font=("30"), command=multiplication)
 diviser = tk.Button(racine, text="/",font=("30"), command=division)
