19428857f6e0638a26f954774ac2e08e7c16ef4d
Matteo <matteo.rabache@ens.uvsq.fr>
2021-01-06 23:30:22 +0100

    save 23

--- calculatrice.py
+++ calculatrice.py
@@ -232,38 +232,47 @@ number0 = tk.Button(racine, text="0", font=(
     "50"), command=lambda: nb0(0), padx=23.5)
 number0.grid(column=0, row=5, columnspan=2)
 racine.bind('<KeyPress-0>', nb0)
+
 number1 = tk.Button(racine, text="1", font=(
     "30"), command=lambda: nb1(1), padx=23.5)
 number1.grid(column=0, row=2, columnspan=2)
 racine.bind("<KeyPress-1>", nb1)
+
 number2 = tk.Button(racine, text="2", font=(
     "50"), command=lambda: nb2(2), padx=23.5)
 number2.grid(column=2, row=2, columnspan=2)
 racine.bind("<KeyPress-2>", nb2)
+
 number3 = tk.Button(racine, text="3", font=(
     "30"), command=lambda: nb3(3), padx=23.5)
 number3.grid(column=4, row=2, columnspan=2)
 racine.bind("<KeyPress-3>", nb3)
+
 number4 = tk.Button(racine, text="4", font=(
     "30"), command=lambda: nb4(4), padx=23.5)
 number4.grid(column=0, row=3, columnspan=2)
 racine.bind("<KeyPress-4>", nb4)
+
 number5 = tk.Button(racine, text="5", font=(
     "30"), command=lambda: nb5(5), padx=23.5)
 number5.grid(column=2, row=3, columnspan=2)
 racine.bind("<KeyPress-5>", nb5)
+
 number6 = tk.Button(racine, text="6", font=(
     "30"), command=lambda: nb6(6), padx=23.5)
 number6.grid(column=4, row=3, columnspan=2)
 racine.bind("<KeyPress-6>", nb6)
+
 number7 = tk.Button(racine, text="7", font=(
     "30"), command=lambda: nb7(7), padx=23.5)
 number7.grid(column=0, row=4, columnspan=2)
 racine.bind("<KeyPress-7>", nb7)
+
 number8 = tk.Button(racine, text="8", font=(
     "30"), command=lambda: nb8(8), padx=23.5)
 number8.grid(column=2, row=4, columnspan=2)
 racine.bind("<KeyPress-8>", nb8)
+
 number9 = tk.Button(racine, text="9", font=(
     "30"), command=lambda: nb9(9), padx=23.5)
 number9.grid(column=4, row=4, columnspan=2)
@@ -273,21 +282,27 @@ racine.bind("<KeyPress-9>", nb9)
 egal = tk.Button(racine, text="=", font=(
     "30"), command=lambda: calcul("="), padx=23.5)
 egal.grid(column=6, row=2, columnspan=4)
+
 plus = tk.Button(racine, text="+", font=("30"),
                  command=lambda: addition("+"), padx=23.5)
 plus.grid(column=6, row=3, columnspan=2)
+
 moins = tk.Button(racine, text="-", font=("30"),
                   command=lambda: soustraction("-"), padx=23.5)
 moins.grid(column=6, row=4, columnspan=2)
+``
 multiplications = tk.Button(racine, text="x", font=(
     "30"), command=lambda: multiplication("*"), padx=23.5)
 multiplications.grid(column=6, row=5, columnspan=2)
+
 diviser = tk.Button(racine, text="/", font=("30"),
                     command=lambda: division("/"), padx=25)
 diviser.grid(column=4, row=5, columnspan=2)
+
 flotant = tk.Button(racine, text=".", font=(
     "30"), command=lambda: nbrFlotant("."), padx=23.5)
 flotant.grid(column=2, row=5, columnspan=2)
+
 effacer = tk.Button(racine, text="Effacer", font=("30"),
                     command=cancel, padx=145)
 effacer.grid(column=1, row=1, columnspan=8)
